3bc4ff92710a229256912639ee5f71fbed701571
mariyasada <mariyasada2406@gmail.com>
2022-10-08 20:26:38 +0530

    feat:added files for webpack configuration

---
+++ src/App.js
@@ -0,0 +1,4 @@
+const App=({name})=>{
+return `<h2> welcome ${name}</h2>`;
+}
+export default App;
---
+++ src/index.js
@@ -0,0 +1,3 @@
+import App from "./App";
+const root=document.getElementById("root");
+root.innerHTML=`<h1> trying to learn web tooling </h1> ${App({name:"Mariya"})}`;
